37aed63135ed0951075e19bdf304a76f9fdb69d8
JasonLandbridge <jlandbrug@protonmail.com>
2019-09-07 16:58:02 +0200

    Added Caliburn Micro and confirmed it works

---
+++ Models/CalculatorData.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TribalWars2_CalculationTools.Annotations;
+
+namespace TribalWars2_CalculationTools.Models
+{
+    public class CalculatorData : INotifyPropertyChanged
+    {
+        private int _atkSpear;
+        public int AtkSpear
+        {
+            get => _atkSpear;
+            set
+            {
+                if (_atkSpear != value)
+                {
+                    _atkSpear = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public CalculatorData()
+        {
+
+        }
+
+        public int DefSpear { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ViewModels/ShellViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Caliburn.Micro;
+using TribalWars2_CalculationTools.Models;
+
+namespace TribalWars2_CalculationTools.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private CalculatorData _selectedCalculatorData;
+        private BindableCollection<CalculatorData> _calculatorData = new BindableCollection<CalculatorData>();
+
+        public ShellViewModel()
+        {
+            CalculatorData.Add(new CalculatorData
+            {
+                AtkSpear = 999,
+                DefSpear = 888
+            });
+            SelectedCalculatorData = CalculatorData[0];
+        }
+
+        public BindableCollection<CalculatorData> CalculatorData
+        {
+            get => _calculatorData;
+            set => _calculatorData = value;
+        }
+
+        public CalculatorData SelectedCalculatorData
+        {
+            get => _selectedCalculatorData;
+            set
+            {
+                _selectedCalculatorData = value;
+                NotifyOfPropertyChange(() => SelectedCalculatorData);
+            }
+        }
+    }
+}
